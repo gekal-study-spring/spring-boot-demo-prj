396dee755b48d0a18f549db9d81f6fcebd7106b4
gekal <liu.hongying@hotmail.com>
2018-08-15 16:23:02 +0900

    ----- Init -----

---
+++ src/main/java/cn/gekal/demo/springbootdemoprj/SpringBootDemoPrjApplication.java
@@ -0,0 +1,12 @@
+package cn.gekal.demo.springbootdemoprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoPrjApplication.class, args);
+	}
+}
